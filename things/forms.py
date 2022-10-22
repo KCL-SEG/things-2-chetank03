316501d270abe0f5f2681eb6e3354a4dbfbf415b
Chetan Krishna Kodeboyina <chetan.kodeboyina@kcl.ac.uk>
2022-10-22 19:14:42 +0100

    Create form and extend home view

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,11 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class ThingForm(forms.Form):
+    name = forms.CharField(label = 'Name', max_length =  35)
+    description = forms.CharField(label = 'Description', max_length = 120, widget=forms.Textarea)
+    quantity = forms.IntegerField(label = 'Quantity', validators=[MinValueValidator(0),MaxValueValidator(50)])
